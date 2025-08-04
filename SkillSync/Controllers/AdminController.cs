e1a581a0d2918d23b367df5503cd9121ea195562
symca07 <symca07@server1.dom>
2025-08-04 14:51:42 +0530

    mergeing the final version of skillsync

---
+++ SkillSync/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization; // Required for authorization
+
+namespace SkillSync.Controllers // Make sure this matches your project's namespace
+{
+    // This attribute will eventually be used to ensure only admins can access these pages.
+    // We will set up roles later in the backend phase.
+    // [Authorize(Roles = "Admin")] 
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
